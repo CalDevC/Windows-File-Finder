268973face279810e4752982cbf0e8a159f6fe5e
WUPQTA - (Chase A) <chasealexander@ussupi.com>
2022-01-27 07:54:07 -0800

    Abstracted the different search types

--- main.ps1
+++ main.ps1
@@ -6,35 +6,25 @@ Set-Content -Path ".\$detected" -Value ""
 Set-Content -Path ".\$notDetected" -Value ""
 Set-Content -Path ".\$tempFound" -Value ""
 
-# #Get info from user about the type of search
-# Write-Host "Enter the search type (R - recursive OR L - local): "
-# $searchType = Read-Host
+#Get info from user about the type of search and validate
+Write-Host "Enter the search type (R - recursive OR L - local): "
+$searchType = Read-Host
 
-# Write-Host "Enter the search term type (R - range OR P - phrase): "
-# $searchTermType = Read-Host
-
-#Get info from user about the search
-Write-Host "Enter the path to the Network folder do you want to search (begin with \\): "
-$folder = Read-Host
-
-Write-Host "Enter the keyword phrase: "
-$phrase = Read-Host
-
-Write-Host "Enter the types of files to search as a comma seperated list (no spaces): "
-$fileTypes = Read-Host
-
-#For each provided file type
-foreach ($type in $fileTypes.Split(",")) {
-  $foundFiles = Get-ChildItem -Path "${folder}\*$type" -Name -Include *$phrase*
-  Add-Content -Path ".\$tempFound" -Value $foundFiles
+if ($searchType -ne "R" -AND $searchType -ne "r" -AND $searchType -ne "L" -AND $searchType -ne "l") {
+  Write-Host "Invalid search type entered"
 }
 
-#Check if no files were found
-if (Get-Content -Path ".\$tempFound" == "") {
-  Add-Content -Path ".\$notDetected" -Value $searchterm
+Write-Host "Enter the search term type (R - range OR P - phrase): "
+$searchTermType = Read-Host
+
+if ($searchTermType -eq "P" -OR $searchTermType -eq "p") {
+  . .\phraseSearch.ps1
+}
+elseif ($searchTermType -eq "P" -OR $searchTermType -eq "p") {
+  . .\rangeSearch.ps1
 }
 else {
-  Add-Content -Path ".\$detected" -Value $detected
+  Write-Host "Invalid search term type entered"
 }
 
 
---
+++ phraseSearch.ps1
@@ -0,0 +1,23 @@
+#Get info from user about the search
+Write-Host "Enter the path to the Network folder do you want to search (begin with \\): "
+$folder = Read-Host
+
+Write-Host "Enter the keyword phrase: "
+$phrase = Read-Host
+
+Write-Host "Enter the types of files to search as a comma seperated list (no spaces): "
+$fileTypes = Read-Host
+
+#For each provided file type
+foreach ($type in $fileTypes.Split(",")) {
+  $foundFiles = Get-ChildItem -Path "${folder}\*$type" -Name -Include *$phrase*
+  Add-Content -Path ".\$tempFound" -Value $foundFiles
+}
+
+#Check if no files were found
+if (Get-Content -Path ".\$tempFound" -eq "") {
+  Add-Content -Path ".\$notDetected" -Value $searchterm
+}
+else {
+  Add-Content -Path ".\$detected" -Value $detected
+}
